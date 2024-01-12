1536b36ff7fda06c517493d5377d5ac217c1b368
Nikar0 <49345980+Nikar0@users.noreply.github.com>
2024-01-12 11:02:54 +0000

    Update AccessControl.sol

--- contracts/interfaces/AccessControl.sol
+++ contracts/interfaces/AccessControl.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.19;
+pragma solidity 0.8.19;
 
 /**
 @notice Based on solmate's Owned.sol with added access control for an operator & error code + if instead of requires.
@@ -15,6 +15,7 @@ abstract contract AccessControl {
 
     event OwnershipTransferred(address indexed user, address indexed newOwner);
     event SetStrategist(address indexed user, address indexed newStrategist);
+    event SetHarvester(address indexed user, address indexed newHarvester);
 
     /*//////////////////////////////////////////////////////////////
                             OWNERSHIP STORAGE
@@ -24,6 +25,7 @@ abstract contract AccessControl {
     address public strategist;
     address public constant treasury = address(0xE37058057B0751bD2653fdeB27e8218439e0f726);
     address public constant devMultiSig = address(0x3522f55fE566420f14f89bd46820EC66D3A5eb7c);
+    address internal harvester = address(0xDFAA88D5d068370689b082D34d7B546CbF393bA9);
 
     modifier onlyOwner() virtual {
         checkOwner();
@@ -32,6 +34,11 @@ abstract contract AccessControl {
 
     modifier onlyAdmin() virtual {
         checkAdmin();
+        _;
+    }
+
+    modifier harvesters() virtual {
+        checkHarvesters();
         _;
     }
 
@@ -59,12 +66,21 @@ abstract contract AccessControl {
         emit SetStrategist(msg.sender, strategist);
     }
 
+    function setHarvester(address _newHarvester) external virtual onlyAdmin {
+        harvester = _newHarvester;
+        emit SetHarvester(msg.sender, _newHarvester);
+    }
+
     function checkOwner() internal virtual {
         if(tx.origin != owner){revert NoAuth();}
     }
 
     function checkAdmin() internal virtual {
-        if(tx.origin != owner && tx.origin != strategist){revert NoAuth();}
+        if(tx.origin != owner || tx.origin != strategist){revert NoAuth();}
+    }
+
+    function checkHarvesters() internal virtual{
+        if(tx.origin != harvester || tx.origin != strategist || tx.origin != owner){revert NoAuth();}
     }
 
 }
